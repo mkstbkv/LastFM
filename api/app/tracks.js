cdddc6c7d6eaf171bfe833f0424bf8e84ec7bbf9
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-03-04 18:53:18 +0600

    исправил ошибки при создании пользователя

--- api/app/tracks.js
+++ api/app/tracks.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Track = require("../models/Track");
+const Album = require("../models/Album");
+const Artist = require("../models/Artist");
 
 const router = express.Router();
 
@@ -10,12 +12,78 @@ router.get("/", async (req, res, next) => {
         if (req.query.album) {
             query.album = req.query.album;
         }
+
+        if (req.query.artist) {
+            const allTracks = await Track.find().populate(
+                {
+                    path:  "album"  ,
+                    match: {
+                        'artist':  req.query.artist
+                    } ,
+                    populate: {
+                        path:  'artist',
+                        select: 'name'
+                    }
+                }
+            );
+
+            const tracks = [];
+
+            for (const tracksKey of allTracks) {
+                if (tracksKey.album !== null) {
+                    tracks.push(tracksKey)
+                }
+            }
+
+            return res.send(tracks);
+
+        }
+
         const tracks = await Track.find(query).populate("album", "title artist");
         return res.send(tracks);
     } catch(e) {
         next(e);
     }
 });
+
+router.get("/byAlbum/:albumID", async (req, res, next) => {
+    try {
+        const allTracks = await Track.find().populate(
+            {
+                path:  "album"  ,
+                match: {
+                    'artist':  req.query.artist
+                } ,
+                populate: {
+                    path:  'artist',
+                    select: 'name'
+                }
+            }
+        );
+
+        const tracks = [];
+
+        for (const tracksKey of allTracks) {
+            if (tracksKey.album !== null) {
+                tracks.push(tracksKey)
+            }
+        }
+        const album = await Album.findById(req.params.albumID);
+        const artist = await Artist.findById(album.artist._id);
+
+
+        return res.send({
+            artist: artist,
+            album: album,
+            tracks: tracks
+        });
+
+    } catch(e) {
+        next(e);
+    }
+});
+
+
 
 router.post("/", async (req, res, next) => {
     try {
